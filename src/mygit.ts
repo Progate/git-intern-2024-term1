54d3fec571611094c26e3b1f1996b6dd0f3efe75
Huyu2239 <huyu2239@gmail.com>
2024-09-09 16:35:44 +0900

    add: GitCommitObjectを使う

--- src/mygit.ts
+++ src/mygit.ts
@@ -2,6 +2,7 @@ import { isGitRepositoryValid } from "./checks.js";
 import { GitRepositoryDestroyedError } from "./errors.js";
 import { hello } from "./hello.js";
 import { isFileExists, readTextFile } from "./utils.js";
+import { GitCommitObject } from "./models.js";
 
 export const mygit = async (argv: Array<string>): Promise<void> => {
   console.log(hello());
@@ -38,13 +39,14 @@ const log = async (): Promise<void> =>{
 
   // read the commit object hash from the ref file
   let commitObjectHash: string = "";
+  let branchName: string | undefined = undefined;
   if (headText.includes("ref: refs/heads/")) {
     const refPath = headText.slice(5, -1).trim();
+    branchName = refPath.split("/").pop();
     try {
       commitObjectHash = await readTextFile(`.git/${refPath}`);
     } catch (error) {
       if (!(await isFileExists(`.git/${refPath}`))) {
-        const branchName = refPath.split("/").pop();
         console.error(`現在のブランチ' ${branchName}' にはまだコミットがありません`);
       } else {
         console.error("コミットオブジェクトのハッシュ値の取得中に予期せぬエラーが発生しました:", error);
@@ -56,4 +58,10 @@ const log = async (): Promise<void> =>{
     commitObjectHash = headText.trim();
   }
   console.log("最新のコミットオブジェクトのハッシュ値:", commitObjectHash);
+  const commitObject = await GitCommitObject.build(commitObjectHash);
+  console.log(commitObject.hash);
+  console.log(`commit ${commitObject.hash}${branchName ? ( ` (HEAD -> ${branchName})`):("")} `);
+  // if (commitObject.parent) {
+  //   console.log(`parent: ${commitObject.parent.hash.slice(0, 7)} ${commitObject.hash.slice(0, 7)}`);
+  // }
 }
