1db24309cf4d07b9a0fa3bf072fd163ac8790185
Huyu2239 <huyu2239@gmail.com>
2024-09-09 16:35:21 +0900

    add: GitCommitObjectクラス&その他クラス - GitCommitObject - GitTreeObject - GitBlobObject

---
+++ src/models.ts
@@ -0,0 +1,65 @@
+import { readZlibFile } from "./utils.js"
+
+
+export class GitCommitObject {
+  path: string
+  tree: string
+  parent: GitCommitObject | undefined
+  author: string
+  committer: string
+  message: string
+  constructor(
+    public hash: string,
+  ) {
+    this.hash = ""
+    this.path = ""
+    this.tree = ""
+    this.parent = undefined
+    this.author = ""
+    this.committer = ""
+    this.message = ""
+  }
+  public static async build(hash: string): Promise<GitCommitObject>{
+    const gitCommitObject = new GitCommitObject(hash);
+    gitCommitObject.hash = hash
+    gitCommitObject.path = `.git/objects/${hash.slice(0, 2)}/${hash.slice(2)}`
+    const planeText: string = await readZlibFile(gitCommitObject.path);
+    console.log(planeText.split("\n"))
+    // for (const line of planeText.split("\n")) {
+    //   if (line.startsWith("tree")) {
+    //     gitCommitObject.tree = line.split(" ")[1]
+    //   } else if (line.startsWith("parent")) {
+    //     gitCommitObject.parent = new GitCommitObject(line.split(" ")[1])
+    //   } else if (line.startsWith("author")) {
+    //     gitCommitObject.author = line.slice(7)
+    //   } else if (line.startsWith("committer")) {
+    //     gitCommitObject.committer = line.slice(10)
+    //   } else {
+    //     gitCommitObject.message = line
+    //   }
+    // }
+
+    gitCommitObject.tree = ""
+    gitCommitObject.parent = undefined
+    gitCommitObject.author = ""
+    gitCommitObject.committer = ""
+    gitCommitObject.message = ""
+    return gitCommitObject;
+  }
+}
+
+export class GitTreeObject {
+  constructor(
+    public hash: string,
+  ) {
+    this.hash = hash
+  }
+}
+
+export class GitBlobObject {
+  constructor(
+    public hash: string,
+  ) {
+    this.hash = hash
+  }
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,8 @@
 import { promises as fs } from "fs";
 import { readFile } from "fs/promises";
 
+import * as zlib from 'zlib';
+
 export const isFileExists = async (path: string): Promise<boolean> => {
   try {
     await fs.access(path);
@@ -23,6 +25,17 @@ export const readTextFile = async (filePath: string): Promise<string> => {
   try {
     const data = await readFile(filePath, "utf-8");
     return data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const readZlibFile = async (filePath: string): Promise<string> => {
+  let result = "";
+  const data = await readFile(filePath);
+  try {
+    result = zlib.unzipSync(data).toString();
+    return result;
   } catch (err) {
     throw err;
   }
