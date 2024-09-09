194a4e486bac40a07bbf552c50042121866280bb
ardRiriy <hynnkgw27@gmail.com>
2024-09-09 16:01:31 +0900

    不要行を削除

--- src/mygit.ts
+++ src/mygit.ts
@@ -1,8 +1,10 @@
-import { hello } from "./hello.js";
+import { log } from "./log.js";
 
 export const mygit = async (argv: Array<string>): Promise<void> => {
-  console.log(hello());
-  console.log(argv);
+  if(argv[2] == "log") {
+    console.log(log(argv));
+    return;
+  }
 
   // Avoid eslint error by adding some async operation.
   await new Promise((resolve) => setTimeout(resolve, 1000));
