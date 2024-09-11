e83aeb13a59fabc81f3ae3c95be7bafdee49f1cd
ardRiriy <hynnkgw27@gmail.com>
2024-09-11 18:01:38 +0900

    簡単に実装できる部分をサクッと書いた

--- src/models/commit.ts
+++ src/models/commit.ts
@@ -1,4 +1,6 @@
+import { getGitPath } from "../utils.js";
 import { Tree } from "./tree.js";
+import * as fs from "node:fs";
 
 export class Commit {
   tree: Tree;
@@ -8,11 +10,18 @@ export class Commit {
   message: string;
   hash: string;
 
-  constructor() {
+  constructor(message: string) {
     this.tree = new Tree();
+    const config = fs
+      .readFileSync((getGitPath(process.cwd()) + "config"))
+      .toString();
+
+
+    console.log(config);
     this.author = "";
-    this.createdAt = 0;
-    this.message = "";
+    // unixtime(秒単位)
+    this.createdAt =  Math.floor(Date.now() / 1000);
+    this.message = message;
     this.hash = "";
   }
 }
