4d0550ab9a6312b178e3dffaad75b49f40ca3869
Huyu2239 <huyu2239@gmail.com>
2024-09-11 13:18:19 +0900

    feat: .git/indexの本文からsha-1のハッシュ値を計算するメソッドを実装

---
+++ src/models/index.test.ts
@@ -0,0 +1,22 @@
+import assert from "node:assert";
+import { describe, it } from "node:test";
+
+import { readFile } from "node:fs/promises";
+import { Index } from "./index.js";
+
+describe("index", () => {
+  it("should print hash of index body", async (t) => {
+    const mockedLog = t.mock.method(console, "log");
+
+    // 本リポジトリの .git/index の本文のハッシュ値を計算する
+    const index = new Index();
+    const indexCheckSumHash = await index.calcCheckSum();
+    console.log(indexCheckSumHash);
+
+    // .git/index に記載されたハッシュ値を取得する
+    const rawIndexContents = await readFile(".git/index");
+    const expectedResult = rawIndexContents.subarray(-20).toString('hex');
+
+    assert.strictEqual(mockedLog.mock.calls[0]?.arguments[0], expectedResult);
+  });
+});
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,6 @@
+import { createHash } from "crypto";
+import { readFile } from "node:fs/promises";
+
 import { Entry } from "./entry.js";
 
 export class Index {
@@ -7,5 +10,13 @@ export class Index {
   constructor() {
     this.entries = [];
     this.entryCount = this.entries.length;
+  }
+
+  async calcCheckSum(): Promise<string> {
+    const rawIndexContents = await readFile(".git/index");
+    const bodyData = rawIndexContents.subarray(0, -20);
+    const hash = createHash("sha1");
+    hash.update(bodyData);
+    return hash.digest("hex");
   }
 }
