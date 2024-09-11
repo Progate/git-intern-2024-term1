639275db72a9e1790dd85cc221f6a8589a17dc9d
ardRiriy <hynnkgw27@gmail.com>
2024-09-11 13:15:41 +0900

    コンストラクタの実装

--- src/models/blob.ts
+++ src/models/blob.ts
@@ -1,9 +1,27 @@
+import { exit } from "process";
+import { uncompressZlib } from "../utils.js";
+import { createHash } from "crypto";
+
 export class Blob {
   content: string;
   hash: string;
 
-  constructor() {
-    this.content = "";
-    this.hash = "";
+  constructor(path: string) {
+    // blobのオブジェクトファイルを読み込む
+    const data = uncompressZlib(path);
+
+    const hasher = createHash("sha1");
+    this.hash = path.split('/').slice(-2).join('');
+    hasher.update(data);
+    if(this.hash != hasher.digest('hex')) {
+      console.error(`Invalid Hash: ${path}`);
+      exit(1);
+    }
+
+    // レスポンスは"blob <文字数>\x00<content>"という形式なので、\x00までを取り除く
+    this.content = data
+      .split('\x00')
+      .slice(1)
+      .join('');
   }
 }
