9637b34bcb6b663f7e1cfe1e988d357513e3b93e
ardRiriy <hynnkgw27@gmail.com>
2024-09-11 15:45:52 +0900

    hashの計算以外のコードを書いた

--- src/models/tree.ts
+++ src/models/tree.ts
@@ -1,24 +1,78 @@
+import * as fs from "node:fs";
+
+import { getGitPath } from "../utils.js";
 import { Blob } from "./blob.js";
+import { Index } from "./index.js";
+
+const index = new Index();
 
 export class Tree {
   directories: Array<Directory>;
   files: Array<File>;
   hash: string;
 
-  constructor() {
+  constructor(path?: string) {
     this.directories = [];
     this.files = [];
+
+    const currentDir = path ? path : getGitPath(process.cwd()) + "../";
+    const files = fs.readdirSync(currentDir);
+
+    files.forEach((fileName) => {
+      const fullPathToFile = currentDir + fileName;
+      const stats = fs.statSync(fullPathToFile);
+      if (stats.isDirectory()) {
+        // Direcotryなら再帰
+        const dir: Directory = {
+          kind: "Directory",
+          direcotry: new Tree(fullPathToFile),
+          fileName: fullPathToFile,
+          authority: stats.mode,
+        };
+
+        if (dir.direcotry.files.length !== 0) {
+          this.directories.push(dir);
+        }
+      } else {
+        // Fileなので、追跡対象ならfilesに加える
+        if (
+          index.entries.some(
+            (entry) => entry.device === stats.dev && entry.inode === stats.ino,
+          )
+        ) {
+          const file: File = {
+            kind: "File",
+            blob: new Blob(fullPathToFile),
+            fileName: fileName,
+            authority: stats.mode,
+          };
+          this.files.push(file);
+        }
+      }
+    });
     this.hash = "";
   }
+
+  // public generateObjectcontent(): string {
+  //   const items = [...this.files, ...this.directories].sort((x, y) => x.fileName.localeCompare(y.fileName));
+  //   let res = [];
+  //   for (const item of items) {
+  //     if(item.kind === "Directory") {
+  //       res.push(`${item.authority} blob ${item.direcotry.hash}} `)
+  //     }
+  //   }
+  // }
 }
 
 export interface File {
+  kind: "File";
   blob: Blob;
   fileName: string;
   authority: number;
 }
 
 export interface Directory {
+  kind: "Directory";
   direcotry: Tree;
   fileName: string;
   authority: number;
--- src/mygit.ts
+++ src/mygit.ts
@@ -1,8 +1,12 @@
 import { hello } from "./hello.js";
+import { uncompressZlib } from "./utils.js";
 
 export const mygit = async (argv: Array<string>): Promise<void> => {
   console.log(hello());
   console.log(argv);
+
+  console.log(uncompressZlib("/Users/ardririy/repos/tmp/.git/objects/6a/30361b9e397c5c30a6938786323dd0e58c54a2")
+    .split('\x00')[1]);
 
   // Avoid eslint error by adding some async operation.
   await new Promise((resolve) => setTimeout(resolve, 1000));
