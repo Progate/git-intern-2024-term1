cb8266b79ff06d790f19b731a91de5b2cfceca33
ardRiriy <hynnkgw27@gmail.com>
2024-09-12 10:21:17 +0900

    lint and format

--- src/models/tree.ts
+++ src/models/tree.ts
@@ -1,12 +1,12 @@
+import * as crypto from "crypto";
 import * as fs from "node:fs";
-import * as crypto from 'crypto';
 
 import { getGitPath } from "../utils.js";
 import { Blob } from "./blob.js";
 import { Index } from "./index.js";
 
 const index = new Index();
-await index.build('.git/index');
+await index.build(".git/index");
 
 export class Tree {
   directories: Array<Directory>;
@@ -22,11 +22,11 @@ export class Tree {
     files.forEach((fileName) => {
       const fullPathToFile = currentDir + fileName;
       const stats = fs.statSync(fullPathToFile);
-      if (stats.isDirectory() && fileName !== '.git') {
+      if (stats.isDirectory() && fileName !== ".git") {
         // Direcotryなら再帰
         const dir: Directory = {
           kind: "Directory",
-          direcotry: new Tree(fullPathToFile + '/'),
+          direcotry: new Tree(fullPathToFile + "/"),
           fileName: fileName,
           authority: 16384, // 固定値
         };
@@ -37,14 +37,12 @@ export class Tree {
         }
       } else {
         // Fileなので、追跡対象ならfilesに加える
-        const item = index
-          .entries
-          .find(
-            (entry) => entry.inode === stats.ino,
-          );
+        const item = index.entries.find((entry) => entry.inode === stats.ino);
         if (item) {
-          const suffix = item.hashForBlob.slice(0, 2) + "/" + item.hashForBlob.slice(2);
-          const pathToBlobObject = getGitPath(process.cwd()) + "objects/" + suffix;
+          const suffix =
+            item.hashForBlob.slice(0, 2) + "/" + item.hashForBlob.slice(2);
+          const pathToBlobObject =
+            getGitPath(process.cwd()) + "objects/" + suffix;
           const file: File = {
             kind: "File",
             blob: new Blob(pathToBlobObject),
@@ -57,46 +55,45 @@ export class Tree {
     });
 
     const content = this.generateObjectContent();
-    this.hash = crypto.createHash('sha1')
-      .update(content)
-      .digest('hex');
-
-    // console.log(this);
+    this.hash = crypto.createHash("sha1").update(content).digest("hex");
   }
 
   public generateObjectContent(): Buffer {
-    const items: Array<{mode: string; name: string; hash: string }> = [];
+    const items: Array<{ mode: string; name: string; hash: string }> = [];
     for (const file of this.files) {
-      const fileMode = file.authority.toString(8).padStart(6, '0');
+      const fileMode = file.authority.toString(8).padStart(6, "0");
       const fileHash = file.blob.hash;
       items.push({ mode: fileMode, name: file.fileName, hash: fileHash });
     }
 
     for (const dir of this.directories) {
-      const dirMode = '40000';
+      const dirMode = "40000";
       const dirHash = dir.direcotry.hash;
       items.push({ mode: dirMode, name: dir.fileName, hash: dirHash });
     }
-    const contentBuffer = Buffer.concat(items
-      .sort((a, b) => a.name.localeCompare(b.name))
-      .map(
-      item => {
-        // modeとnameをASCIIバイト列に変換し、ヌル文字（\0）で区切る
-        const header = Buffer.from(`${item.mode} ${item.name}\0`, 'utf-8');
-        // ハッシュをバイナリとして変換
-        const hashBuffer = Buffer.from(item.hash, 'hex');
+    const contentBuffer = Buffer.concat(
+      items
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map((item) => {
+          const header = Buffer.from(`${item.mode} ${item.name}\0`, "utf-8");
+          const hashBuffer = Buffer.from(item.hash, "hex");
+          return Buffer.concat([header, hashBuffer]);
+        }),
+    );
 
-        // 結合して返す
-        return Buffer.concat([header, hashBuffer]);
-      }
-    ));
+    const headerBuffer = Buffer.from(
+      `tree ${contentBuffer.length.toString()}\0`,
+      "utf-8",
+    );
+    const contentWithHeaderBuffer = Buffer.concat([
+      headerBuffer,
+      contentBuffer,
+    ]);
 
-    // 全体のバイナリデータにツリーの長さとヌル文字を含むヘッダーを付ける
-    const headerBuffer = Buffer.from(`tree ${contentBuffer.length.toString()}\0`, 'utf-8');
-    const contentWithHeaderBuffer = Buffer.concat([headerBuffer, contentBuffer]);
+    // デバッグ用: 一つ下の階層にバイナリデータをファイルに書き込み
+    // レポジトリルートでやるとレポジトリにoutput.binが絶対含まれないので安全(.gitignoreには含まれないので)
+    // fs.writeFileSync('../output.bin', contentWithHeaderBuffer);
 
-    // バイナリデータをファイルに書き込み
-    fs.writeFileSync('output.bin', contentWithHeaderBuffer);
     return contentWithHeaderBuffer;
   }
 }
