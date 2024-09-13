7aa4989c5765c13deece804c5013528e8dc0d333
Huyu2239 <huyu2239@gmail.com>
2024-09-13 11:05:40 +0900

    add: blobオブジェクトの作成処理

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,26 +1,14 @@
-import crypto from "node:crypto";
 import { stat } from "node:fs/promises";
-import { readFile } from "node:fs/promises";
 
+import { Blob } from "../models/blob.js";
 import { Entry } from "../models/entry.js";
 import { Index } from "../models/index.js";
 import { doesFileExist, getGitPath } from "../utils.js";
 
-const genBlobSha1 = async (filePath: string): Promise<string> => {
-  const file = await readFile(filePath);
-  const content = file.toString();
-  const header = `blob ${content.length.toString()}\0`;
-  const store = header + content;
-  const shasum = crypto.createHash("sha1");
-  shasum.update(store);
-  const sha1 = shasum.digest("hex");
-
-  return sha1;
-};
-
 const getEntryFromFile = async (addingFilePath: string): Promise<Entry> => {
   const fileStat = await stat(addingFilePath, { bigint: true });
-  const blobHash = await genBlobSha1(addingFilePath);
+  const blob = new Blob();
+  const blobHash = await blob.dump(addingFilePath);
   return {
     ctimeSec: Number((fileStat.ctime.getTime() / 1000).toFixed(0)),
     ctimeNanoSec: Number(fileStat.ctimeNs % 1000000000n),
