4e453cca0973de06e04b65693ae1c8110d7a6261
Huyu2239 <huyu2239@gmail.com>
2024-09-13 11:02:16 +0900

    add: Blobクラスにblobを書き出すメソッドの追加

--- src/models/blob.ts
+++ src/models/blob.ts
@@ -1,7 +1,8 @@
 import { createHash } from "crypto";
+import * as fs from "fs/promises";
 import { exit } from "process";
 
-import { uncompressZlib } from "../utils.js";
+import { getGitPath, uncompressZlib } from "../utils.js";
 
 export class Blob {
   content: string;
@@ -25,5 +26,23 @@ export class Blob {
 
     // レスポンスは"blob <文字数>\x00<content>"という形式なので、\x00までを取り除く
     this.content = data.split("\x00").slice(1).join("");
+  }
+
+  public async dump(filePath: string): Promise<string> {
+    this.content = (await fs.readFile(filePath, "utf-8")).toString();
+    const store = `blob ${this.content.length.toString()}\x00${this.content}`;
+    const hash = createHash("sha1");
+    hash.update(store);
+    const sha1 = Buffer.from(hash.digest("hex"), "hex");
+
+    const blobObjPath =
+      getGitPath(process.cwd()) +
+      "objects/" +
+      sha1.subarray(0, 2).toString("hex") +
+      "/" +
+      sha1.subarray(2).toString("hex");
+    const blobBuffer = Buffer.concat([Buffer.from(store), sha1]);
+    await fs.writeFile(blobObjPath, blobBuffer);
+    return sha1.toString("hex");
   }
 }
