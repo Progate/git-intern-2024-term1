a5f4a5c964a774901807dd63ce66d61a1ee701eb
Huyu2239 <huyu2239@gmail.com>
2024-09-13 14:39:02 +0900

    fix: コマンドによっての分岐を修正

--- src/mygit.ts
+++ src/mygit.ts
@@ -4,23 +4,30 @@ import { add } from "./commands/add.js";
 import { commit } from "./commands/commit.js";
 
 export const mygit = async (argv: Array<string>): Promise<void> => {
-  if (argv[2] === "add") {
-    const addingFilePath = argv[3];
-    if (!addingFilePath) {
-      console.error("Please specify the file path to add");
-      return;
-    }
-    await add(addingFilePath);
+  if (!argv[2]) {
+    console.error("Usage: mygit <command> [<args>]");
+    exit(1);
   }
-
-  if (argv[2] === "commit") {
-    if (!argv[3]) {
-      console.error(
-        "Usage: git commit <msg> <name(optional)> <email(optional)>",
-      );
-      exit(1);
-    }
-    commit(argv[3], argv[4], argv[5]);
+  const commandName = argv[2];
+  switch (commandName) {
+    case "add":
+      if (!argv[3]) {
+        console.error("Please specify the file path to add");
+        return;
+      }
+      await add(argv[3]);
+      break;
+    case "commit":
+      if (!argv[3]) {
+        console.error(
+          "Usage: git commit <msg> <name(optional)> <email(optional)>",
+        );
+        exit(1);
+      }
+      commit(argv[3], argv[4], argv[5]);
+      break;
+    default:
+      console.error(`Unknown command: ${commandName}`);
   }
   // Avoid eslint error by adding some async operation.
   await new Promise((resolve) => setTimeout(resolve, 1000));
