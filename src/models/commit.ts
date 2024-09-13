bf2bd9529519e3d45ec6765d25379a1e3cde0c79
ardririy <is0681sv@ed.ritsumei.ac.jp>
2024-09-13 14:34:41 +0900

    既存のcommit objectからのロードに対応 (#21)

--- src/models/commit.ts
+++ src/models/commit.ts
@@ -1,12 +1,18 @@
 import * as crypto from "crypto";
 import * as fs from "fs";
 import * as os from "os";
+import { exit } from "process";
 
-import { compressZlib, fetchHeadHash, getGitPath } from "../utils.js";
+import {
+  compressZlib,
+  fetchHeadHash,
+  getGitPath,
+  uncompressZlib,
+} from "../utils.js";
 import { Tree } from "./tree.js";
 
 export class Commit {
-  tree: Tree;
+  tree?: Tree;
   parent?: string;
   author: string;
   createdAt: number;
@@ -14,7 +20,43 @@ export class Commit {
   hash: string;
   email: string;
 
-  constructor(message: string, username?: string, email?: string) {
+  constructor() {
+    this.author = "";
+    this.createdAt = 0;
+    this.message = "";
+    this.hash = "";
+    this.email = "";
+  }
+
+  public load(hash: string): void {
+    const prefix = hash.slice(0, 2);
+    const suffix = hash.slice(2);
+
+    const path = getGitPath(process.cwd()) + `objects/${prefix}/${suffix}`;
+    const data = uncompressZlib(path);
+
+    const content = data.split("\x00")[1];
+    if (!content) {
+      console.error("Failed to read commit object");
+      exit(1);
+    }
+
+    this.hash = hash;
+    for (const line of content.split("\n")) {
+      if (line.startsWith("parent")) {
+        this.parent = line.slice("parent ".length);
+      } else if (line.startsWith("author")) {
+        const arr = line.split(" ");
+        if (arr[1]) this.author = arr[1];
+        if (arr[2]) this.email = arr[2].slice(1, -1);
+        if (arr[3]) this.createdAt = parseInt(arr[3]);
+      }
+    }
+
+    this.message = content.split("\n\n")[1]?.slice(0, -1) ?? "";
+  }
+
+  public build(message: string, username?: string, email?: string): void {
     this.tree = new Tree();
     this.author = username ?? os.userInfo().username;
     this.email = email ?? os.hostname();
@@ -30,7 +72,7 @@ export class Commit {
 
   generateContent(): Buffer {
     const res: Array<Buffer> = [];
-    res.push(Buffer.from(`tree ${this.tree.hash}\x0a`, "utf-8"));
+    res.push(Buffer.from(`tree ${this.tree?.hash ?? ""}\x0a`, "utf-8"));
     if (this.parent) {
       res.push(Buffer.from(`parent ${this.parent}\x0a`, "utf-8"));
     }
